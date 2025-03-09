bc85f6bc8f853a4f8ad76732eba0527a182052b4
SamanthaEllen <ellen0310.se@hotmail.com>
2025-03-08 22:03:13 -0300

    Finalizando projeto de estudo, um  blog de filmes com links do meu GitHub e Linkedin

---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+ 
+ @Component({
+   selector: 'app-small-card',
+   imports: [
+    RouterModule,
+   ],
+   templateUrl: './small-card.component.html',
+   styleUrls: ['./small-card.component.scss']
+ })
+ export class SmallCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string = ""
+
+  @Input()
+  cardTitle:string = ""
+
+  @Input()
+  Id:string="0"
+   
+   constructor() { }
+ 
+   ngOnInit(): void {
+   }
+ 
+ }
